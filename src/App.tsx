65136302cffcc36e0e4cd2f4c3f51bba9ecb1d41
satoshi-eric <satoshi.eric@gmail.com>
2022-05-21 14:50:29 -0300

    upload de arquivo feito

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,29 @@ const App = () => {
   const csvUrl = 'https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/iris.csv'
   // const csvUrl = 'https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv'
   const [data, setData] = React.useState<d3.DSVRowArray<string>>()
+  const [state, setState] = React.useState<boolean>(false)
+  
   React.useEffect(() => {
     d3.csv(csvUrl).then(res => { setData(res) })
   }, [])
 
+  const uploadFile = (event: React.ChangeEvent<HTMLInputElement>) => {
+    let files = event.target.files
+    if (files && files[0]) {
+      files[0].text().then(
+        res => setData(d3.csvParse(res))
+      )
+    }
+    setState(!state)
+  }
+
   if (!data) return <div>Loading...</div>
   
   return (
-    <TableLens data={data}/>
+    <div>
+      <input type="file" name="file" id="file" onChange={uploadFile} />
+      <TableLens data={data}/>
+    </div>
   );
 }
 
